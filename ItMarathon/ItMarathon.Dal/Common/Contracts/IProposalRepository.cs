fd5734288b36770878191310005caada69a6876b
booond <maksbond03@gmail.com>
2024-10-02 22:51:04 +0300

    first commit

---
+++ ItMarathon/ItMarathon.Dal/Common/Contracts/IProposalRepository.cs
@@ -0,0 +1,40 @@
+﻿using ItMarathon.Dal.Entities;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ItMarathon.Dal.Common.Contracts;
+
+/// <summary>
+/// Interface for managing Proposal entities in the repository.
+/// </summary>
+public interface IProposalRepository : IRepositoryBase<Proposal>
+{
+    /// <summary>
+    /// Retrieves list of Proposals entities.
+    /// </summary>
+    /// <param name="trackChanges">Indicates whether to track changes in the Entity Framework context.</param>
+    /// <param name="queryOptions">The OData query options for filtering, sorting, and paging.</param>
+    /// <param name="top">The number of proposals to take (page size).</param>
+    /// <param name="skip">The number of proposals to skip (page offset).</param>
+    /// <returns>A paginated list of Proposal entities.</returns>
+    Task<DataPage<Proposal>> GetProposalsAsync(bool trackChanges, ODataQueryOptions queryOptions, int? top, int? skip);
+
+    /// <summary>
+    /// Retrieves a Proposal entity by its ID.
+    /// </summary>
+    /// <param name="proposalId">The ID of the proposal.</param>
+    /// <param name="trackChanges">Indicates whether to track changes in the Entity Framework context.</param>
+    /// <returns>The Proposal entity, or null if not found.</returns>
+    Task<Proposal?> GetProposalAsync(long proposalId, bool trackChanges);
+
+    /// <summary>
+    /// Creates a new Proposal entity.
+    /// </summary>
+    /// <param name="proposal">The Proposal entity.</param>
+    void CreateProposal(Proposal proposal);
+
+    /// <summary>
+    /// Deletes a Proposal entity.
+    /// </summary>
+    /// <param name="proposal">The Proposal entity.</param>
+    void DeleteProposal(Proposal proposal);
+}
---
+++ ItMarathon/ItMarathon.Dal/Repositories/ProposalRepository.cs
@@ -0,0 +1,71 @@
+﻿using ItMarathon.Dal.Common;
+using ItMarathon.Dal.Common.Contracts;
+using ItMarathon.Dal.Context;
+using ItMarathon.Dal.Entities;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItMarathon.Dal.Repositories;
+
+public class ProposalRepository(ApplicationDbContext repositoryContext) :
+    RepositoryBase<Proposal>(repositoryContext), IProposalRepository
+{
+    public async Task<DataPage<Proposal>> GetProposalsAsync(
+        bool trackChanges,
+        ODataQueryOptions queryOptions,
+        int? top,
+        int? skip)
+    {
+        IQueryable<Proposal> query = FindAll(trackChanges);
+
+        if (queryOptions != null)   
+        {
+            query = (IQueryable<Proposal>)queryOptions.ApplyTo(query);
+        }
+
+        var totalProposalsCount = await query.CountAsync();
+        
+        if (queryOptions?.OrderBy == null)
+        {
+            query = query.OrderBy(p => p.Id);
+        }
+
+        if (skip.HasValue)
+        {
+            query = query.Skip(skip.Value);
+        }
+        
+        if (top.HasValue)
+        {
+            query = query.Take(top.Value);
+        }
+
+        query = query
+            .Include(p => p.AppUser)
+            .Include(p => p.Photos)
+            .Include(p => p.Properties!)
+                .ThenInclude(properties => properties.PropertyDefinition)
+            .Include(p => p.Properties!)
+                .ThenInclude(properties => properties.PredefinedValue)
+                    .ThenInclude(prop => prop!.ParentPropertyValue);
+
+        var proposals = await query.ToListAsync();
+        
+        return new DataPage<Proposal>(proposals, totalProposalsCount);
+    }
+
+    public async Task<Proposal?> GetProposalAsync(long proposalId, bool trackChanges)
+        => await FindByCondition(c => c.Id.Equals(proposalId), trackChanges)
+        .Include(p => p.AppUser)
+        .Include(p => p.Photos)
+        .Include(p => p.Properties!)
+            .ThenInclude(properties => properties.PropertyDefinition)
+        .Include(p => p.Properties!)
+            .ThenInclude(properties => properties.PredefinedValue)
+                .ThenInclude(prop => prop!.ParentPropertyValue)
+        .SingleOrDefaultAsync();
+
+    public void CreateProposal(Proposal proposal) => Create(proposal);
+
+    public void DeleteProposal(Proposal proposal) => Delete(proposal);
+}
